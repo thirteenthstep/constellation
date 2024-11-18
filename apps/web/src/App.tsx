d7b7ba50df8a1694877808c4e2bba8a39740583c
Thomas Kröniger <tkroeniger@gmail.com>
2024-11-18 19:39:37 +0100

    add shared core package for native and web, add react-native-web

---
+++ apps/web/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import HelloWorld from '@constellation/app/components/HelloWorld';
+
+function App() {
+
+  return (
+    <>
+      <HelloWorld />
+    </>
+  );
+}
+
+export default App;
---
+++ packages/app/components/HelloWorld.tsx
@@ -0,0 +1,12 @@
+import {Text, View} from 'react-native';
+import React from 'react';
+
+const HelloWorld = () => {
+  return (
+    <View style={{backgroundColor: 'white'}}>
+      <Text>Hello World</Text>
+    </View>
+  );
+};
+
+export default HelloWorld;
