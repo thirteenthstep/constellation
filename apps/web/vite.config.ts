a4c541af30ade003b5f1d7f619f275e6a0da08b8
Thomas Kröniger <tkroeniger@gmail.com>
2024-11-21 15:37:43 +0100

    update vite config, add tamagui plugin

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,25 +1,38 @@
+import {tamaguiPlugin} from '@tamagui/vite-plugin';
 import react from '@vitejs/plugin-react';
 import {defineConfig} from 'vite';
 
+
 const extensions = [
-  '.web.tsx',
-  '.tsx',
-  '.web.ts',
-  '.ts',
-  '.web.jsx',
-  '.jsx',
   '.web.js',
+  '.web.jsx',
+  '.web.ts',
+  '.web.tsx',
+  '.mjs',
   '.js',
-  '.css',
+  '.mts',
+  '.ts',
+  '.jsx',
+  '.tsx',
   '.json',
 ];
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tamaguiPlugin({
+      config: 'src/tamagui.config.ts',
+      components: ['tamagui'],
+      optimize: true,
+    }),
+  ],
   optimizeDeps: {
     esbuildOptions: {
       resolveExtensions: extensions,
+      loader: {
+        '.js': 'jsx',
+      },
     },
   },
   resolve: {
@@ -29,6 +42,8 @@ export default defineConfig({
     extensions,
   },
   define: {
-    __DEV__: process.env.NODE_ENV !== 'PRODUCTION',
+    __DEV__: `${process.env.NODE_ENV === 'development'}`,
+    DEV: `${process.env.NODE_ENV === 'development'}`,
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
   },
 });
