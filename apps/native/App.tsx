3584942e433eb696b7935a20d87e384c77b64feb
Thomas Kröniger <tkroeniger@gmail.com>
2024-11-21 18:51:42 +0100

    patch some annoying errors of html renderer library

--- apps/native/App.tsx
+++ apps/native/App.tsx
@@ -5,17 +5,9 @@
  * @format
  */
 import React from 'react';
-import {LogBox} from 'react-native';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 
 import {Providers} from '@constellation/app/providers/Providers.tsx';
-
-
-LogBox.ignoreLogs([
-  /TNodeChildrenRenderer/,
-  /MemoizedTNodeRenderer/,
-  /TRenderEngineProvider/,
-]);
 
 function App() {
   return (
